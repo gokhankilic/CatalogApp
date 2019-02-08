e825e12d4d985b7ae6fd2a2f2b31af044193699a
Gökhan Kılıç <gokhankilic@Gokhans-MacBook-Air.local>
2019-02-08 11:49:34 +0300

    Initial Commit

---
+++ Dev-ProfileTests/Dev_ProfileTests.swift
@@ -0,0 +1,34 @@
+//
+//  Dev_ProfileTests.swift
+//  Dev-ProfileTests
+//
+//  Created by Gökhan Kılıç on 8.02.2019.
+//  Copyright © 2019 Gökhan Kılıç. All rights reserved.
+//
+
+import XCTest
+@testable import Dev_Profile
+
+class Dev_ProfileTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
